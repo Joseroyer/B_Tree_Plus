7d5d6e243fb24076c8303feb1f54d6a08243b91c
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2025-05-25 18:19:34 -0300

    exibir elementos folha, ajuste split No

--- B_Tree_plus/src/main/java/org/example/BTree.java
+++ B_Tree_plus/src/main/java/org/example/BTree.java
@@ -3,6 +3,10 @@ package org.example;
 public class BTree {
     private No raiz;
 
+    public BTree() {
+        raiz = null;
+    }
+
     public void inserir(int info, int posArq) {
         No folha, pai;
         int pos;
@@ -10,18 +14,22 @@ public class BTree {
             raiz = new No(info, posArq);
         } else {
             folha = navegarAteFolha(info);
-            pos = folha.procurarPosicao(info);
-            folha.remanejar(pos);
+            if(folha != null){
+                pos = folha.procurarPosicao(info);
+                folha.remanejar(pos);
 
-            folha.setvInfo(pos, info);
-            folha.setvPos(pos, posArq);
-            folha.setTl(folha.getTl() + 1);
+                folha.setvInfo(pos, info);
+                folha.setvPos(pos, posArq);
+                folha.setTl(folha.getTl() + 1);
 
-            if (folha.getTl() > No.n - 1)// verifica se está cheio
-            {
-                pai = localizarPai(folha, info);
-                split(folha, pai);
-            }
+                if (folha.getTl() > No.n - 1)// verifica se está cheio
+                {
+                    pai = localizarPai(folha, info);
+                    split(folha, pai);
+                }
+            }else
+                System.out.println("Problema nessa inserção" + info);
+
         }
     }
 
@@ -36,17 +44,26 @@ public class BTree {
         if (isFolha) {
             valorCalc = (int) Math.ceil((No.n - 1) / 2.0);
 
-            for (int i = valorCalc; i < folha.getTl(); i++) {
+            for (int i = valorCalc; i < No.n; i++) {
                 cx2.setvInfo(i - valorCalc, folha.getvInfo(i));
                 cx2.setvPos(i - valorCalc, folha.getvPos(i));
                 cx2.setvLig(i - valorCalc, folha.getvLig(i));
                 cx2.setTl(cx2.getTl() + 1);
             }
             cx2.setvLig(No.n - valorCalc, folha.getvLig(No.n));
+
         } else {
             valorCalc = (int) Math.ceil((No.n / 2.0) - 1);
 
+            for (int i = valorCalc + 1; i < No.n; i++) {
+                cx2.setvInfo(i - (valorCalc + 1), folha.getvInfo(i));
+                cx2.setvPos(i - (valorCalc + 1), folha.getvPos(i));
+                cx2.setvLig(i - (valorCalc + 1), folha.getvLig(i));
+                cx2.setTl(cx2.getTl() + 1);
+            }
+            cx2.setvLig(No.n - (valorCalc + 1), folha.getvLig(No.n));
         }
+
         for (int i = 0; i < valorCalc; i++) {
             cx1.setvInfo(i, folha.getvInfo(i));
             cx1.setvPos(i, folha.getvPos(i));
@@ -55,9 +72,21 @@ public class BTree {
         }
         cx1.setvLig(valorCalc, folha.getvLig(valorCalc));
 
-        //Ligar as folhas com Cx1 e Cx2
 
+        if (isFolha) {
+            //Ligar as folhas com Cx1 e Cx2
 
+            cx1.setProx(cx2);
+            cx1.setAnt(folha.getAnt());
+
+            cx2.setAnt(cx1);
+            cx2.setProx(folha.getProx());
+
+            if (folha.getAnt() != null)
+                folha.getAnt().setProx(cx1);
+            if (folha.getProx() != null)
+                folha.getProx().setAnt(cx2);
+        }
 
         if (folha == pai) {
             folha.setvInfo(0, folha.getvInfo(valorCalc));
@@ -106,5 +135,40 @@ public class BTree {
             no = no.getvLig(pos);
         }
         return pai;
+    }
+
+    public void exibirFolha() {
+        System.out.println("## Exibir Folhas ##");
+
+        No folha = raiz;
+        while (folha.getvLig(0) != null) //Desce até a folha
+            folha = folha.getvLig(0);
+
+        while (folha != null) {
+
+            for (int i = 0; i < folha.getTl(); i++) {
+                System.out.print(folha.getvInfo(i) + " ");
+            }
+            System.out.print("\t");
+            folha = folha.getProx();
+        }
+    }
+    public void in_ordem()
+    {
+        System.out.println("## Exibir In-Ordem##");
+        in_ordem(raiz);
+    }
+
+    private void in_ordem(No raiz)
+    {
+        if (raiz != null)
+        {
+            for(int i=0; i<raiz.getTl(); i++)
+            {
+                in_ordem(raiz.getvLig(i));
+                System.out.print(raiz.getvInfo(i));
+            }
+            in_ordem(raiz.getvLig(raiz.getTl()));
+        }
     }
 }
--- B_Tree_plus/src/main/java/org/example/Main.java
+++ B_Tree_plus/src/main/java/org/example/Main.java
@@ -6,18 +6,25 @@ public class Main {
     public static void main(String[] args) {
         BTree bt = new BTree();
 
+        //exemplo de Aula
         bt.inserir(1, 1);
         bt.inserir(4, 4);
         bt.inserir(7, 7);
         bt.inserir(10, 10);
         bt.inserir(17, 17);
         bt.inserir(21, 21);
-//        bt.inserir(31, 31);
-//        bt.inserir(25, 25);
-//        bt.inserir(19, 19);
-//        bt.inserir(20, 20);
-//        bt.inserir(28, 28);
-//        bt.inserir(42, 42);
+        bt.inserir(31, 31);
+        bt.inserir(25, 25);
+        bt.inserir(19, 19);
+        bt.inserir(20, 20);
+        bt.inserir(28, 28);
+        bt.inserir(42, 42);
 
+
+//        for (int i=0; i<100; i++)
+//            bt.inserir(i, i);
+
+        bt.exibirFolha();
+        bt.in_ordem();
     }
 }
